3a2f4e4d1b8139ea545a2b4cf5d2035cb26e5343
Husen Zhang <hxz116@gmail.com>
2017-01-31 14:28:00 -0500

    now takes stdin stdout

---
+++ transpose_tsv.py
@@ -0,0 +1,11 @@
+import csv
+import sys
+
+# the transposed file has an extra row 'taxonomy'
+
+infile = sys.stdin
+outfile = sys.stdout
+reader = csv.reader(infile, delimiter = '\t')
+next(reader, None)  # skip header, return None
+transReader = zip(*reader) # unzip is same as transpose
+csv.writer(outfile, delimiter = '\t').writerows(transReader)
