2f0440eade29cd5af27c9c2fb2db6925d3cf5dfb
Husen Zhang <hxz116@gmail.com>
2017-02-07 11:47:55 -0500

    started documentation

--- transpose_tsv.py
+++ transpose_tsv.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+""" transpose a tsv file from stdin 
+remove the header and write to stdout
+"""
 
 import csv
 import sys
@@ -7,6 +10,7 @@ import sys
 
 infile = sys.stdin
 outfile = sys.stdout
+
 reader = csv.reader(infile, delimiter = '\t')
 next(reader, None)  # skip header, return None
 transReader = zip(*reader) # unzip is same as transpose
