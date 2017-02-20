48db7be7ca71a4d0dc0983488bc3c1a05257522e
husenzhang <hxz116@gmail.com>
2017-02-20 16:06:43 -0500

    plot histogram or bar

--- stdin2pdf.R
+++ stdin2pdf.R
@@ -1,12 +1,17 @@
 #!/usr/bin/env Rscript
 
-library('ggplot2')
+library(ggplot2)
 
 d = read.delim(file("stdin"), header=F)
 print(names(d))
+
+if ( length(names(d)) == 2 ) {
 p = ggplot(d, aes(V1, V2)) + 
           geom_bar(stat = 'identity') +
           theme_bw() +
           coord_flip()
+} else {
+p = ggplot(d, aes(V1)) + geom_histogram()
+}  
 
-ggsave('plot.pdf', plot=p)
+ggsave('plot.pdf', plot=p, width=5, height=5)
